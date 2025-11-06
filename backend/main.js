e385eaa7936b1a503e46ea8abc423de68569a0c3
Xiaojie Li <xiaoli5253@gmail.com>
2025-11-06 01:31:47 -0500

    Added getBrowser() so that the browser can be reused to optimze speed

--- backend/main.js
+++ backend/main.js
@@ -3,27 +3,42 @@ const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer')
 
 // To Do: Return error if user enters an invalid URL. If the function returns an error, display an error on the website
+
+let browser;
+
+// Allow the browser to be reused
+async function getBrowser() {
+  if (!browser) {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage'
+        ]
+    });
+  }
+  return browser;
+}
+
 async function priceFind(url){
     try{
-    const browser = await puppeteer.launch({
-        headless:true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox'
-        ]
-    }); 
-    const page = await browser.newPage();
-    await page.goto(url);
-    const priceTag = await page.evaluate(() => { // PriceTag stores the latest price of the item
-        const price = document.querySelector(".fr-ec-price").textContent.replace("$", ""); // Price is stored in .fr-ec-price, extracts the price then cleans it up
-        return parseFloat(price); // Convert price to float
-    });
-    await browser.close();
-    return priceTag        
+        const browser = await getBrowser()
+        const page = await browser.newPage();
+        await page.goto(url);
+        const priceTag = await page.evaluate(() => { // PriceTag stores the latest price of the item
+            const price = document.querySelector(".fr-ec-price").textContent.replace("$", ""); // Price is stored in .fr-ec-price, extracts the price then cleans it up
+            return parseFloat(price); // Convert price to float
+        });
+        await page.close(); // Close the page instead of the browser
+        return priceTag        
     } catch(error){
         console.log("Error getting the price", error)
     }
 }
+// Shuts down the browser
+process.on('exit', async () => { if (browser) await browser.close(); });
+process.on('SIGINT', async () => { if (browser) await browser.close(); });
+process.on('SIGTERM', async () => { if (browser) await browser.close(); });
 
 async function sendEmail(){
     const transporter = nodemailer.createTransport({
@@ -50,4 +65,3 @@ async function runEmail(){
 }
  
 module.exports = {priceFind}
-
